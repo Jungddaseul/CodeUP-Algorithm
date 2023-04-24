cb8fabe0b5361dfd873ef0eea5679afb05b7bb7a
Jung Daseul <114555218+Jungddaseul@users.noreply.github.com>
2023-04-24 16:12:40 +0900

    Create 3120_리모컨.py

---
+++ 그리디/3120_리모컨.py
@@ -0,0 +1,15 @@
+﻿cur, target = map(int, (input().split()))
+
+result = 0
+
+result += abs(cur-target)//10
+c = abs(cur-target) % 10
+
+if (c==1 or c==5):
+    result += 1
+elif (c==2 or c==4 or c==6 or c==9):
+    result += 2
+elif (c==3 or c==7 or c==8):
+    result += 3
+
+print(result)
