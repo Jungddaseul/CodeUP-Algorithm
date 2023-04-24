640834dba270ee10413756d5fe1df15102e977c7
Jung Daseul <114555218+Jungddaseul@users.noreply.github.com>
2023-04-24 15:33:25 +0900

    Create 2001_최소대금.py

---
+++ 2001_최소대금.py
@@ -0,0 +1,19 @@
+﻿pasta = []
+juice = []
+
+for i in range(3):
+    pasta.append(int(input()))
+    
+for i in range(2):
+    juice.append(int(input()))
+    
+min_price = pasta[0] + juice[0]
+
+for i in pasta:
+    for j in juice:
+        if i + j < min_price:
+            min_price = i + j
+
+print("%.1f" %(min_price*1.1))
+
+    
